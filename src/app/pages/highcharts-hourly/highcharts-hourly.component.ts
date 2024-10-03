e122fd10bf335758d51f9d2a7efc82453ad31051
Asadbek <devasadbek@gmail.com>
2024-10-03 22:55:26 +0500

    zooming improved

--- src/app/pages/highcharts-hourly/highcharts-hourly.component.ts
+++ src/app/pages/highcharts-hourly/highcharts-hourly.component.ts
@@ -3,7 +3,9 @@ import * as Highcharts from 'highcharts';
 import { HighchartsChartModule } from 'highcharts-angular';
 import { WeatherService } from '../../core/services/weather.service';
 import { Hourly } from '../../shared/models/weather.model';
-import moment from 'moment';
+import StockModule from 'highcharts/modules/stock';
+
+StockModule(Highcharts);
 
 @Component({
   selector: 'app-highcharts-hourly',
@@ -15,14 +17,24 @@ import moment from 'moment';
 export class HighchartsHourlyComponent implements OnInit {
   Highcharts: typeof Highcharts = Highcharts;
   chartOptions: WritableSignal<Highcharts.Options> = signal({
+    rangeSelector: {
+      selected: 1,
+    },
+    navigator: {
+      enabled: true,
+    },
     chart: {
-      type: 'line',
       zooming: {
         type: 'x',
+        enabled: true,
       },
     },
     title: {
       text: 'Weather Data',
+    },
+    xAxis: {
+      type: 'datetime',
+      minRange: 5 * 3600 * 1000,
     },
     yAxis: [
       {
@@ -70,14 +82,10 @@ export class HighchartsHourlyComponent implements OnInit {
     this.weatherService.getHourlyForecast().subscribe(res => {
       if (res) {
         const data = this.getTemperatureSeries(res.list);
-        const labels = this.getHourlyLabels(res.list);
         const humidity = this.getHourlyHumidity(res.list);
         this.chartOptions.update(value => {
           return {
             ...value,
-            xAxis: {
-              categories: labels,
-            },
             series: [
               {
                 name: 'Temperature',
@@ -100,19 +108,13 @@ export class HighchartsHourlyComponent implements OnInit {
 
   private getTemperatureSeries(data: Hourly[]) {
     return data.map(hourly => {
-      return hourly.main.temp;
+      return [hourly.dt * 1000, hourly.main.temp];
     });
   }
 
   private getHourlyHumidity(data: Hourly[]) {
     return data.map(hourly => {
-      return hourly.main.humidity;
-    });
-  }
-
-  private getHourlyLabels(data: Hourly[]) {
-    return data.map(hourly => {
-      return moment(hourly.dt * 1000).format('HH:mm');
+      return [hourly.dt * 1000, hourly.main.humidity];
     });
   }
 }
